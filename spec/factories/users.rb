256df02f929ae181640003c122e1b1d8aa26da8d
SebastianN97 <sebbe132132@gmail.com>
2020-09-09 19:37:00 +0200

    Creating user_can_log_in_spec.rb with test 'user can log in on the page'. Test passes

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    email { "user1@gmail.com" }
+    password { "MyString" }
+  end
+end
---
+++ spec/feature/user_can_log_in_spec.rb
@@ -0,0 +1,36 @@
+
+
+feature 'User can log in on the page' do
+  let(:user) {FactoryBot.create(:user) }
+  context 'successfully' do
+    before do
+      visit root_path
+      click_on "Login"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_on "Log in"
+    end
+  
+    it 'is expected to show successful login message' do
+      expect(page).to have_content 'Signed in successfully.'
+    end
+
+    it 'is expected to redirect the user to the root path' do
+      expect(current_path).to eq root_path
+    end
+  end
+
+  context 'unsuccessfully with wrong password' do
+    before do
+      visit root_path
+      click_on "Login"
+      fill_in "Email", with: user.email
+      fill_in "Password", with: 'Wrong password'
+      click_on "Log in"
+    end
+
+    it 'is expected to show wrong message' do
+      
+    end
+  end
+end
